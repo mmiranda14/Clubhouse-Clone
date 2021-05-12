cc8e9daf1de3b09f87d4ba5c5cacb38d8c1ea0f0
Michael Miranda <mmiranda14@stac.edu>
2021-05-11 22:47:24 -0400

    added btn and router links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
 import Welcome from "./pages/Welcome";
-
+import PlanLayout from "./pages/layouts/PlanLayout";
+import { BrowserRouter } from "react-router-dom";
 // <planLayout>
+
 function App() {
-	return <Welcome />;
+	return (
+		<BrowserRouter>
+			<PlanLayout>
+				<Welcome />
+			</PlanLayout>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import style from "../style/welcome.module.css";
+import { Link } from "react-router-dom";
 
 export default function Welcome() {
 	return (
@@ -18,6 +19,16 @@ export default function Welcome() {
 					join!
 				</p>
 				<p>Michael Miranda & the Clubhouse team</p>
+			</div>
+			<div className={style.actionBtn}>
+				<Link
+					exact
+					to="/get-username"
+					className="primaryBtn d-flex align-items-center mb-3"
+				>
+					Get your username {/* <img src="" alt="" /> */}
+				</Link>
+				<Link>Have an invite text? Sign in</Link>
 			</div>
 		</div>
 	);
